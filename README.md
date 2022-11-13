4551fbd06fa89dca55f3baf48f4839cc0beb0797
seintpl <95775405+seintpl@users.noreply.github.com>
2022-11-13 20:29:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# imagstodon
+
+A simple javascript tool to search for profiles on Mastodon instances.
+
+Just input a nickname nad wait for the results. If a profile picture is found on a Mastodon instance, it will be displayed.
+
+After clicking "Search", please wait for a couple of seconds - the results will appear in a moment.
+
+
+
+The above tool is for educational purposes only. The author does not promote or encourage any illegal activities.
+Using this tool for illegal activities is strictly prohibited. You are the one responsible for your actions.
---
+++ script.js
@@ -0,0 +1,36 @@
+function findprof(instance,nick) {
+    document.getElementById("profiles").innerHTML += '<div class="col-md-3 text-center pb-3 border"><div style="font-weight:bold">'+instance+'</div><div id="'+instance+'"><br /><br /><br />Profile not found here.';
+    var urljson = 'https://'.concat(instance,'/@',nick,'.json');
+    var http = new XMLHttpRequest();
+        http.open('HEAD', urljson, false);
+        http.send();
+        if (http.status != 404) {
+          fetch(urljson)
+            .then((response) => {
+               return response.json()
+            })
+            .then((data) => {
+               document.getElementById(instance).innerHTML = 'Account-type: '+data["type"]+'<br />Published: '+data["published"].slice(0,10)+'<br />'+data["summary"]+'<a href="'+data["id"]+'"><img src="'+data["icon"]["url"]+'" width="75%" /></a></div></div>';
+            })
+        }
+}    
+
+function imag() {
+  f = document.getElementById("nickname").value;
+  document.getElementById("profiles").innerHTML = '';
+  if (f=='') {document.getElementById("imag_result").innerHTML = '<div style="padding-top: 10px; font-weight:bold;">Enter nickname above.</div>';}
+  else {
+    findprof('mastodon.social',f);
+    findprof('mastodon.online',f);
+    findprof('infosec.exchange',f);
+    findprof('nerdculture.de',f);
+    findprof('toot.wales',f);
+    findprof('hachyderm.io',f);
+    findprof('kolektiva.social',f);
+    findprof('masto.ai',f);
+    findprof('sueden.social',f);
+    findprof('tkz.one',f);
+    findprof('mastodon.cloud',f);
+    findprof('mastodon.vlaanderen',f);
+  } 
+}
