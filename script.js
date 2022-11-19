74124d16f07664e2b58041360d173f09c13504b0
seintpl <95775405+seintpl@users.noreply.github.com>
2022-11-19 14:16:56 +0100

    External instance list + more instances

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ function findprof(instance,nick) {
       })
       .then((data) => {
          try {
+            if (data["id"]) {
+                document.getElementById(instance).innerHTML = '<a href="'+data["id"]+'" target="_blank">Go to profile<br /></a>';
+            }  
+         }
+         catch {
+         }
+         try {
             if (data["type"]) {
-                document.getElementById(instance).innerHTML = 'Account-type: '+data["type"];
+                document.getElementById(instance).innerHTML += 'Account-type: '+data["type"];
                 document.getElementById(instance).parentNode.style.backgroundColor = "#ddffdd";
             }  
          }
@@ -25,48 +32,29 @@ function findprof(instance,nick) {
          catch {
          }         
          try {
-            document.getElementById(instance).innerHTML += '<a href="'+data["id"]+'"><img src="'+data["icon"]["url"]+'" width="75%" /></a>';
+            document.getElementById(instance).innerHTML += '<a href="'+data["id"]+'" target="_blank"><img src="'+data["icon"]["url"]+'" width="75%" /></a>';
          }
          catch {
          } 
          document.getElementById(instance).innerHTML += '</div></div>';
       })
-}    
+}
 
 function imag() {
   f = document.getElementById("nickname").value;
   document.getElementById("profiles").innerHTML = '';
-  if (f=='') {document.getElementById("imag_result").innerHTML = '<div style="padding-top: 10px; font-weight:bold;">Enter nickname above.</div>';}
+  if (f=='') {document.getElementById("profiles").innerHTML = '<div style="padding-top: 10px; font-weight:bold;">Enter nickname above.</div>';}
   else {
-    findprof('mastodon.social',f);
-    findprof('mastodon.online',f);
-    findprof('infosec.exchange',f);
-    findprof('0sint.social',f);
-    findprof('bologna.one',f);
-    findprof('dizl.de',f);
-    findprof('graz.social',f);
-    findprof('hachyderm.io',f);
-    findprof('kolektiva.social',f);
-    findprof('ludosphere.fr',f);
-    findprof('maly.io',f);
-    findprof('mast.dragon-fly.club',f);
-    findprof('masto.ai',f);
-    findprof('mastodon.arch-linux.cz',f);
-    findprof('mastodon.cc',f);
-    findprof('mastodon.cloud',f);
-    findprof('mastodon.eus',f);
-    findprof('mastodon.fun',f);
-    findprof('mastodon.ml',f);
-    findprof('mastodon.nl',f);  
-    findprof('mastodon.uy',f);
-    findprof('mastodon.xyz',f);
-    findprof('mastodon.vlaanderen',f);
-    findprof('mastodon-belgium.be',f);
-    findprof('nerdculture.de',f);
-    findprof('octodon.social',f);
-    findprof('snabelen.no',f);
-    findprof('sueden.social',f);
-    findprof('tkz.one',f);
-    findprof('toot.wales',f);
+    fetch('instances.json')
+    .then((res) => {
+       return res.json()
+    })
+    .then((urldata) => {
+      urldata.instances.forEach(obj => {
+        Object.entries(obj).forEach(([key, value]) => {
+            findprof(`${value}`,f);
+        });
+      });
+    })
   } 
 }
